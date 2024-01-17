0023109b9c2423b5fade7883162f512f3d65055d
alessiovietri <info@alessiovietri.it>
2024-01-17 13:23:40 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script>
+/* 
+    Come faccio ad utilizzare un componente dentro un altro?
+    Si seguono 3 passaggi:
+    1) Importazione del componente
+    2) Dichiarazione del componente all'interno dell'oggetto
+    3) Utilizzo del componente
+*/
+// 1) Importazione
+import AppHeader from './components/AppHeader.vue';
+
+export default {
+    data() {
+        return {
+            count: 0
+        };
+    },
+    // 2) Dichiarazione dei componenti
+    components: {
+        AppHeader
+    }
+};
+</script>
+
+<template>
+    <div>
+        <!-- 3) Utilizzo del componente -->
+        <AppHeader />
+
+        <h1>
+            Ciao ragazzi
+        </h1>
+        <p>
+            {{ count }}
+        </p>
+        <button @click="count++">
+            Incrementa count
+        </button>
+    </div>
+</template>
+
+<style scoped>
+/* Commenti CSS */
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'Ciao ragazzi'
+        };
+    },
+};
+</script>
+
+<template>
+    <header>
+        <h1 class="main-title">
+            {{ title }}
+        </h1>
+    </header>
+</template>
+
+<style scoped>
+    header {
+        padding: 20px;
+        background-color: aquamarine;
+    }
+</style>
